22c0c98f99514c879a95cd12c27d5189604979ac
iqra farah <iqranur542@hotmail.com>
2025-09-06 20:40:28 +0200

    first commit

---
+++ Snake.py
@@ -0,0 +1,81 @@
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TILE_SIZE = 25
+
+WINDOW_WIDTH = TILE_SIZE * COLS
+WINDOW_HEIGHT= TILE_SIZE * ROWS
+
+class Tile:  
+ def __init__(self, x,y):
+     self.x = x
+     self.y = y
+
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+canvas = tkinter.Canvas(window, bg ="black", width= WINDOW_WIDTH, height = WINDOW_HEIGHT, borderwidth = 0, highlightthickness=5)
+canvas.pack()
+window.update()
+
+#center the window
+window_width = window.winfo_width()
+window_height= window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2)- (window_width/2))
+window_y = int((screen_height/2)- (window_height/2))
+#format "(w)x(h)+(x)(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+#initialize game
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) #single tile, snake's head
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+velocityX = 0
+velocityY = 0
+
+def change_direction(e): #e = event
+   #print (e)
+   #print(e.keysym)
+   global velocityX, velocityY
+
+   if(e.keysym =="Up"):
+      velocityX = 0
+      velocityY = 1
+   elif(e.keysym == "Down"):
+      velocityX = 0
+      velocityY = 1
+   elif(e.keysym =="Left"):
+      velocityX = -1
+      velocityY = 0
+   elif(e.keysym =="Right"):
+      velocityX = 1
+      velocityY = 0
+
+def  move():
+   global snake
+
+   snake.x += velocityX * TILE_SIZE
+   snake.y += velocityY * TILE_SIZE
+
+def draw():
+   global snake
+   move()
+
+#draw snake 
+canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
+
+#draw food
+canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill= "red")
+
+window.after(100, draw) #100ms = 1/10 second, 10 frames/second
+
+draw()
+
+window.bind("<KeyRelease>", change_direction)
+window.mainloop()
